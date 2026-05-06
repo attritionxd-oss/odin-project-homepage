f4b856ee8af4461ae88362e3e1d5398769d85995
attritionxd-oss <attritionxd@gmail.com>
2026-05-06 12:24:49 +0800

    feat: add logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import "./logo.css";
+
+export const Logo = {
+  render: () => {
+    return `
+      <div class="header__logo-wrapper">
+        <div class="header__logo"></div><h1>Project: Homepage</h1>
+      </div>
+    `;
+  },
+};
