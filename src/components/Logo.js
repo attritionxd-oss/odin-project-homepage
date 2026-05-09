b915586e0f5bccc58bc714d7ceb73793e166f344
attritionxd-oss <attritionxd@gmail.com>
2026-05-09 14:27:19 +0800

    refactor: logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,14 @@
 import "./logo.css";
+import LogoAsset from "/src/assets/logo.png?sizes[]=40";
 
 export const Logo = {
   render: () => {
     return `
       <div class="header__logo-wrapper">
-        <div class="header__logo"></div><h1>Project: Homepage</h1>
+        <div class="header__logo">
+          <img src="${LogoAsset.src}" />
+        </div>
+        <h1>Project: Homepage</h1>
       </div>
     `;
   },
