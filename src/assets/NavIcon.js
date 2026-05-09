3fc68b958b24c87df023dc9c22f93ad05ab41931
attritionxd-oss <attritionxd@gmail.com>
2026-05-09 15:35:43 +0800

    build: add github icon and project screenshot placeholders

--- src/assets/NavIcon.js
+++ src/assets/NavIcon.js
@@ -9,7 +9,8 @@ export const NavIcon = {
   render: () => {
     return `
       <svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <title>menu</title>
+        <title>Navigation menu</title>
+        <desc>Click this icon to open the navigation menu</desc>
         <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
       </svg>
     `;
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,4 +1,11 @@
 import "./section-2.css";
+import { GitHubIcon } from "/src/assets/GitHubIcon.js";
+import ProjectScreenshot1 from "/src/assets/project-screenshot-1.png?sizes[]=300,sizes[]=400";
+import ProjectScreenshot2 from "/src/assets/project-screenshot-2.png?sizes[]=300,sizes[]=400";
+import ProjectScreenshot3 from "/src/assets/project-screenshot-3.png?sizes[]=300,sizes[]=400";
+import ProjectScreenshot4 from "/src/assets/project-screenshot-4.png?sizes[]=300,sizes[]=400";
+import ProjectScreenshot5 from "/src/assets/project-screenshot-5.png?sizes[]=300,sizes[]=400";
+import ProjectScreenshot6 from "/src/assets/project-screenshot-6.png?sizes[]=300,sizes[]=400";
 
 export const Section2 = {
   render: () => {
@@ -8,54 +15,108 @@ export const Section2 = {
           <h2>Previous projects</h2>
           <div class="projects-wrapper">
             <article class="project-1">
-              <img class="section-2__project-image" alt="project-1-img" />
+              <img
+                class="section-2__project-image"
+                alt="project-1-img"
+                sizes="(max-width: 400) 300px, 400px"
+                src="${ProjectScreenshot1.src}"
+                srcset="${ProjectScreenshot1.srcSet}"
+              />
               <div class="section-2__project-description">
-                <h3>Project name</h3>
+                <div class="section-2__project-description-header">
+                  <h3>Project 1</h3>
+                  ${GitHubIcon.render()}
+                </div>
                 <p>
                   Short description of the project. Just a couple of sentences will do.
                 </p>
               </div>
             </article>
             <article class="project-2">
-              <img class="section-2__project-image" alt="project-2-img" />
+              <img
+                class="section-2__project-image"
+                alt="project-2-img"
+                sizes="(max-width: 400) 300px, 400px"
+                src="${ProjectScreenshot2.src}"
+                srcset="${ProjectScreenshot2.srcSet}"
+              />
               <div class="section-2__project-description">
-                <h3>Project name</h3>
+                <div class="section-2__project-description-header">
+                  <h3>Project 2</h3>
+                  ${GitHubIcon.render()}
+                </div>
                 <p>
                   Short description of the project. Just a couple of sentences will do.
                 </p>
               </div>
             </article>
             <article class="project-3">
-              <img class="section-2__project-image" alt="project-3-img" />
+              <img
+                class="section-2__project-image"
+                alt="project-3-img"
+                sizes="(max-width: 400) 300px, 400px"
+                src="${ProjectScreenshot3.src}"
+                srcset="${ProjectScreenshot3.srcSet}"
+              />
               <div class="section-2__project-description">
-                <h3>Project name</h3>
+                <div class="section-2__project-description-header">
+                  <h3>Project 3</h3>
+                  ${GitHubIcon.render()}
+                </div>
                 <p>
                   Short description of the project. Just a couple of sentences will do.
                 </p>
               </div>
             </article>
             <article class="project-4">
-              <img class="section-2__project-image" alt="project-4-img" />
+              <img
+                class="section-2__project-image"
+                alt="project-4-img"
+                sizes="(max-width: 400) 300px, 400px"
+                src="${ProjectScreenshot4.src}"
+                srcset="${ProjectScreenshot4.srcSet}"
+              />
               <div class="section-2__project-description">
-                <h3>Project name</h3>
+                <div class="section-2__project-description-header">
+                  <h3>Project 4</h3>
+                  ${GitHubIcon.render()}
+                </div>
                 <p>
                   Short description of the project. Just a couple of sentences will do.
                 </p>
               </div>
             </article>
             <article class="project-5">
-              <img class="section-2__project-image" alt="project-5-img" />
+              <img
+                class="section-2__project-image"
+                alt="project-5-img"
+                sizes="(max-width: 400) 300px, 400px"
+                src="${ProjectScreenshot5.src}"
+                srcset="${ProjectScreenshot5.srcSet}"
+              />
               <div class="section-2__project-description">
-                <h3>Project name</h3>
+                <div class="section-2__project-description-header">
+                  <h3>Project 5</h3>
+                  ${GitHubIcon.render()}
+                </div>
                 <p>
                   Short description of the project. Just a couple of sentences will do.
                 </p>
               </div>
             </article>
             <article class="project-6">
-              <img class="section-2__project-image" alt="project-6-img" />
+              <img
+                class="section-2__project-image"
+                alt="project-6-img"
+                sizes="(max-width: 400) 300px, 400px"
+                src="${ProjectScreenshot6.src}"
+                srcset="${ProjectScreenshot6.srcSet}"
+              />
               <div class="section-2__project-description">
-                <h3>Project name</h3>
+                <div class="section-2__project-description-header">
+                  <h3>Project 6</h3>
+                  ${GitHubIcon.render()}
+                </div>
                 <p>
                   Short description of the project. Just a couple of sentences will do.
                 </p>
