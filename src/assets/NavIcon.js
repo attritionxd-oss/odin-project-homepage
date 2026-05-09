cc4a725c109b4779db1ba5bf3fd9ae8d56ca152e
attritionxd-oss <attritionxd@gmail.com>
2026-05-09 14:23:47 +0800

    refactor: add assets

---
+++ src/assets/NavIcon.js
@@ -0,0 +1,17 @@
+import "./nav-icon.css";
+
+/* NavIcon source
+  http://www.w3.org/2000/svg
+  https://pictogrammers.com/library/mdi/icon/menu/
+*/
+
+export const NavIcon = {
+  render: () => {
+    return `
+      <svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <title>menu</title>
+        <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+      </svg>
+    `;
+  },
+};
